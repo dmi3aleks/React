a5e4c22f4cfa35fee89522c5423298bfbd9d38be
dmitry <dmitry@localhost.localdomain>
2018-10-03 19:03:30 +0900

    Fetch price for a given stock

--- simpleApp/src/index.js
+++ simpleApp/src/index.js
@@ -3,58 +3,76 @@ class StockPrice extends React.Component {
     constructor() {
         super();
         this.state = {
-            price: 6988
+            price: 6988,
+            stockCode: 'GOOGL'
         };
-        this.updatePrice = this.updatePrice.bind(this);
+        this.updateStockCode = this.updateStockCode.bind(this);
+        this.updatePrice();
+    }
+
+    updateStockCode(newStockCode) {
+        if (newStockCode === this.state.stockCode) {
+            console.log('Same stock code:' + newStockCode);
+            return;
+        }
+        console.log('New stock code:' + newStockCode);
+        this.setState({
+            stockCode: newStockCode
+        });
+        this.updatePrice();
     }
 
     updatePrice() {
-        this.setState({
-            price: this.state.price + 1
-        });
-    }
-
-    render() {
-       return (
-           <div>
-               <h1>Stock price for {this.props.instrument}: {this.state.price}</h1>
-               <button onClick={this.updatePrice}>Update</button>
-               <Background />
-           </div>
-       )
-    }
-}
-
-class Background extends React.Component {
-
-    constructor() {
-        super();
-        this.state = {
-            prices: [],
-        };
-    }
-
-    componentDidMount() {
-        fetch('https://www.alphavantage.co/query?apikey=UEW52BCFH9Z5FMD7&function=TIME_SERIES_DAILY_ADJUSTED&symbol=GOOGL')
+        console.log('getPrice running: ' + this.state.stockCode);
+        var URL = `https://www.alphavantage.co/query?apikey=UEW52BCFH9Z5FMD7&function=TIME_SERIES_DAILY_ADJUSTED&symbol=${this.state.stockCode}`;
+        console.log(URL);
+        fetch(URL)
             .then(results => { return results.json(); } )
             .then(data => {
                 var timeSeries = data["Time Series (Daily)"];
                 console.log(timeSeries);
                 var prices = [];
                 for(var key in timeSeries) {
-                    console.log(key + " : " + timeSeries[key]["1. open"]);
                     prices.push(key + " : " + timeSeries[key]["1. open"] + " \n");
                 }
-                this.setState({prices:prices});
+                console.log('New price: ' + prices[0]);
+                this.setState({price:prices[0]});
             })
     }
 
     render() {
+       return (
+           <div>
+               <h1>Stock price for {this.state.stockCode}: {this.state.price}</h1>
+               <button onClick={() => this.updatePrice()}>Update</button>
+               <TextInput
+                   onStockCodeUpdate={(stockCode) => this.updateStockCode(stockCode)}/>
+           </div>
+       )
+    }
+}
+
+class TextInput extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: ''
+        }
+    }
+
+    render() {
+
         return (
-            <div className="container">
-                {this.state.prices}
-            </div>
+            <input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
         )
+    }
+
+    updateInputValue(evt) {
+        this.setState({
+            inputValue: evt.target.value
+        });
+        this.props.onStockCodeUpdate(evt.target.value);
     }
 }
 
