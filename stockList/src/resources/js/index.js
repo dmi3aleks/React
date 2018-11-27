a5e4bec934999fadb3c4f53f11385c66ba1f5945
dmitry <dmitry@localhost.localdomain>
2018-11-27 12:20:06 +0900

    Next generation JS

--- stockList/src/resources/js/index.js
+++ stockList/src/resources/js/index.js
@@ -1,17 +1,37 @@
+//import {consideration} from './calc.js'
+
+// Arrow Function for Consideration calculation
+const consideration = (price, volume) => {
+  let cons = price * volume;
+  return cons;
+}
+
+class Instrument {
+  constructor(name, currentPrice) {
+    this.name = name
+    this.currentPrice = currentPrice
+  }
+  getPrice = () => { return this.currentPrice }
+}
+
+let inst = new Instrument('Sony', '7800')
+console.log(inst.getPrice())
+
 function Trade(props) {
   return (
     <div className="trade">
       <h1>{props.instr}</h1>
       <p>Price: {props.price}</p>
+      <p>Consideration: {props.consid}</p>
     </div>
   );
 };
 
 var app = (
   <div>
-    <Trade instr="Sony" price="7800"/>
+    <Trade instr="Sony" price="7800" consid={consideration(7800,10)}/>
     <Trade instr="Sharp" price="5800"/>
-    <Trade instr="NipponRadio" price="3800"/>
+    <Trade instr="NipponRadio" price="8800"/>
   </div>
 );
 
---
+++ stockList/src/resources/js/utilities.js
@@ -0,0 +1,4 @@
+export const consideration = (price, volume) => {
+  let cons = price * volume;
+  return cons;
+}
