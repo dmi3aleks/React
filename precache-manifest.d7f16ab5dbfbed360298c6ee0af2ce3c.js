a7a723775b68b0e94546dcea6c12f0bcd659fb31
dmitry <dmitry@localhost.localdomain>
2018-12-10 10:19:43 +0900

    Updates

---
+++ precache-manifest.d7f16ab5dbfbed360298c6ee0af2ce3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81312d4a81e499095a3d",
+    "url": "/dashboard/static/js/runtime~main.81312d4a.js"
+  },
+  {
+    "revision": "de4ce954e8f391e7f136",
+    "url": "/dashboard/static/js/main.de4ce954.chunk.js"
+  },
+  {
+    "revision": "22a8e7a9a6fdf86046d9",
+    "url": "/dashboard/static/js/1.22a8e7a9.chunk.js"
+  },
+  {
+    "revision": "de4ce954e8f391e7f136",
+    "url": "/dashboard/static/css/main.85a3c9fb.chunk.css"
+  },
+  {
+    "revision": "a7c76f92599e23743ac4e94882430d62",
+    "url": "/dashboard/index.html"
+  }
+];
