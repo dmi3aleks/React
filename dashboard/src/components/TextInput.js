f37983357734d5295c431890f47b82ba5f8a1d5d
dmitryaleks <dmitry@localhost.localdomain>
2018-12-28 14:28:30 +0900

    Prohibit non-numberic input into order quantity and price fields

--- dashboard/src/components/TextInput.js
+++ dashboard/src/components/TextInput.js
@@ -16,7 +16,7 @@ class TextInput extends Component {
             <div className="HorizontalDiv">
               <label className="Label" id="lab" htmlFor="in">{this.props.input_name}: </label>
               <br />
-              <input id={this.props.tag} className="InputOrder" value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)} onBlur={evt => this.submitInputValue(evt)}/>
+              <input id={this.props.tag} className="InputOrder" value={this.state.inputValue} onBlur={evt => this.submitInputValue(evt)} onChange={(evt => this.onChanged(evt))}/>
             </div>
         )
     }
@@ -30,6 +30,20 @@ class TextInput extends Component {
     submitInputValue(evt) {
       this.props.onInputUpdate(this.props.tag, evt.target.value);
     }
+
+    onChanged(evt) {
+        let newText = '';
+        let numbers = '0123456789';
+
+        let text = evt.target.value
+        for (var i=0; i < text.length; i++) {
+
+            if(numbers.indexOf(text[i]) > -1 ) {
+                newText = newText + text[i];
+            }
+        }
+        this.setState({ inputValue: newText });
+	}
 }
 
 export default TextInput;
